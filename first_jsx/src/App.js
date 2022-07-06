f544b45151481918523e0ffee0b6124f0d35c5b4
Yo Han Song <jsong061@gmail.com>
2022-07-06 12:51:12 -0700

    First JSX assignment

---
+++ first_jsx/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import HelloDojo from './components/HelloDojo';
+
+function App() {
+  return (
+    <div className="App">
+      <HelloDojo />
+    </div>
+  );
+}
+
+export default App;
---
+++ first_jsx/src/components/HelloDojo.jsx
@@ -0,0 +1,23 @@
+// IMPORT DEPENDENCY
+import React, {Component} from "react"
+
+
+// CREATE THE COMPONENT
+class HelloDojo extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello Dojo</h1>
+                <h3>Things I need to do:</h3>
+                <p>* Learn React</p>
+                <p>* Climb Mt. Everest</p>
+                <p>* Run a marathon</p>
+                <p>* Feed the dogs</p>
+            </div>
+        ); 
+    }
+}
+
+
+// EXPORT THE COMPONENT
+export default HelloDojo
