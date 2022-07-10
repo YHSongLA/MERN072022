4c474bdb713ae13cbee4a34203ac360bfd2be820
Yo Han Song <jsong061@gmail.com>
2022-07-10 15:01:04 -0700

    Box Generator Assignment

---
+++ box_generator/src/components/DisplayColor.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const DisplayColor = (props) => {
+
+    const {allColors} = props
+
+  return (
+    <div className="d-flex justify-content-evenly align-items-center container">
+        {
+            allColors.map((color, index) => {
+                return (
+                    <div key={index}>
+                        <p style={{backgroundColor: color, width:"100px", height:"100px"}}></p>
+                    </div>
+                )
+            })
+        }
+    </div>
+  )
+}
+
+export default DisplayColor
