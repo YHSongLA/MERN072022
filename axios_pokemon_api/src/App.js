83786e81c48307583502959fd02383447484d56a
Yo Han Song <jsong061@gmail.com>
2022-07-12 15:01:43 -0700

    Axios Pokemon API Assignment

---
+++ axios_pokemon_api/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import AxioPokemon from './components/AxioPokemon';
+
+function App() {
+  return (
+    <div className="App">
+      <AxioPokemon />
+    </div>
+  );
+}
+
+export default App;
